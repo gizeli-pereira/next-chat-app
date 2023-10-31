abc1bdc52d8e12a2d42ecc68cd701fb415fd59c1
gizeli-pereira <gizeli.pereira1@gmail.com>
2023-10-31 14:23:55 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   }
   return (
     <div className="max-w-xl mx-auto border rounded-lg p-10 mt-32">
-      <h1 className="text-4xl font-semibold text-center">Login to use chat!</h1>
+      <h1 className="text-4xl font-semibold text-center">Login to chat</h1>
 
       <div className="mt-8">
         <MainLogin />
